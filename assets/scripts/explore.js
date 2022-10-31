6afa221e977f7d0c04fd18f0141a5db12c2e0fc0
jennifer-jiang <jiangjennifer7@gmail.com>
2022-10-30 17:59:15 -0700

    Add speech synthesis implementation

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  function getVoiceList() {
+    const voices = speechSynthesis.getVoices();
+    const voiceSelect = document.querySelector('select');
+    for (const voice of voices) {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  getVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = getVoiceList;
+  }
+
+  const playButton = document.querySelector("button");
+  playButton.addEventListener('click', function() {
+    const voices = speechSynthesis.getVoices();
+    const voiceSelect = document.querySelector('select');
+    const input = document.getElementById("text-to-speak");
+    const textToSpeak = new SpeechSynthesisUtterance(input.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (const voice of voices) {
+      if (voice.name === selectedOption) {
+        textToSpeak.voice = voice;
+      }
+    }
+    
+    const smileImage = document.getElementById("explore").querySelector("img");
+    textToSpeak.addEventListener('start', function () {
+      smileImage.src="assets/images/smiling-open.png";
+    })
+    textToSpeak.addEventListener('end', function () {
+      smileImage.src="assets/images/smiling.png";
+    })
+    window.speechSynthesis.speak(textToSpeak);
+  })
 }
